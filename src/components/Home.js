3087606f82090e2e2e13f3b74fb949d675f8bfd6
ateebasif <ateeb.asif902@gmail.com>
2022-05-26 16:16:28 +0500

    todo map array and add new item in array setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
-  const users = [
+  const [users, setUsers] = useState([
     { id: 1, name: "John" },
     { id: 2, name: "Ali" },
     { id: 3, name: "Michael" },
     { id: 4, name: "Jane" },
-  ];
+  ]);
+  const [todo, setTodo] = useState("");
 
-  // const mapped = users.map((user) => ({
-  //   userName: user.name,
-  //   userId: user.id,
-  // }));
+  // const onClickUser = (item) => {
+  //   console.log("user", item);
+  // };
 
-  // console.log("mapped", mapped);
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-  const onClickUser = (user) => {
-    console.log("clicked on ", user);
+    // making a copy of users state
+    const allusers = users;
+
+    // creatin an object of new user
+    // Math.random() is a javascript function to generate a random number
+    const newUser = { id: Math.random(), name: todo };
+
+    // adding the new user object in the allusers array
+    allusers.push(newUser);
+
+    // updating or setting the users state with addition of new user
+    setUsers([...allusers]);
+    // reseting or clearing the input field for todo
+    setTodo("");
   };
+
+  console.log("users", users);
 
   return (
     <div>
       <div className="container">
-        {users.map((user) => (
-          <div key={user.id} className="item">
-            <div onClick={() => onClickUser(user)}>{user.name}</div>
-          </div>
-        ))}
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit">add</button>
+        </form>
+
+        <div>
+          {users.map((user) => {
+            return (
+              <div
+                key={user.id}
+                className="item"
+                // onClick={() => onClickUser(user)}
+              >
+                {user.name}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
